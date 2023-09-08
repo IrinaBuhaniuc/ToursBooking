e5550ce73db65b6a4879b2537ae0ee52a5242e54
Irina <ipotereanu@gmail.com>
2023-09-08 18:49:15 +0200

    user class was registred

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from . import models
+
+class CustomUser(UserAdmin):
+    #adjustments
+    ...
+
 
 # Register your models here.
+admin.site.register(models.User, CustomUser)
