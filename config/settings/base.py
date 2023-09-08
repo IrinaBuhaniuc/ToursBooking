a7a6bc133cb79f7e092367815cb23d0aad036377
Irina <ipotereanu@gmail.com>
2023-09-08 18:50:22 +0200

    abstract user added in base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -118,4 +118,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'user.User'
